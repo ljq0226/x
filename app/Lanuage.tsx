a460edb0a16dcb82d66d1f3f1bae541cad252d25
ljq0226 <1035821043@qq.com>
2023-07-27 10:30:46 +0800

    add: ui 组件

---
+++ app/Lanuage.tsx
@@ -0,0 +1,23 @@
+'use client'
+import useTranslate from '../hooks/useTranslate'
+import i18n from '@/lib/i18n'
+
+function Lanuage() {
+  const lngs: any = {
+    en: { nativeName: 'English' },
+    zh: { nativeName: '中文' },
+  }
+  const t = useTranslate()
+  return (
+    <select onChange={(evt) => {
+      i18n.changeLanguage(evt.target.value)
+    }}>
+      {Object.keys(lngs).map(lng => (
+        <option key={lng} value={lng} label={lngs[lng].nativeName}
+          style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} />
+      ))}
+    </select>
+  )
+}
+
+export default Lanuage
