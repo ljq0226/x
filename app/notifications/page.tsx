12eef39c2abf72c03bb99cbc170c70a21e857011
ljq0226 <1035821043@qq.com>
2023-07-28 20:54:42 +0800

    add: new page

---
+++ app/notifications/page.tsx
@@ -0,0 +1,13 @@
+import { Sidebar } from '@/components/sidebar/SideBar'
+import Home from '@/components/home/Home'
+import Aside from '@/components/aside/Aside'
+
+export default function App() {
+  return (
+    <div className="flex justify-center w-full h-screen gap-0 lg:gap-4">
+      <Sidebar />
+      <Home />
+      <Aside />
+    </div>
+  )
+}
